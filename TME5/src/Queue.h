f08314ddfe00610126caf7eb03ac7d229d9cc404
liu-weida <117468158+liu-weida@users.noreply.github.com>
2023-10-23 16:07:13 +0200

    Add files via upload

--- TME5/src/Queue.h
+++ TME5/src/Queue.h
@@ -3,63 +3,89 @@
 
 #include <cstdlib>
 #include <mutex>
+#include <condition_variable>
 
 namespace pr {
 
-// MT safe version of the Queue, non blocking.
-template <typename T>
-class Queue {
-	T ** tab;
-	const size_t allocsize;
-	size_t begin;
-	size_t sz;
-	mutable std::mutex m;
+    template <typename T>
+    class Queue {
+        T ** tab;
+        const size_t allocsize;
+        size_t begin;
+        size_t sz;
+        mutable std::mutex m;
+        std::condition_variable cond_vide;
+        std::condition_variable cond_plein;
+        bool isBlocking;
 
-	// fonctions private, sans protection mutex
-	bool empty() const {
-		return sz == 0;
-	}
-	bool full() const {
-		return sz == allocsize;
-	}
-public:
-	Queue(size_t size) :allocsize(size), begin(0), sz(0) {
-		tab = new T*[size];
-		memset(tab, 0, size * sizeof(T*));
-	}
-	size_t size() const {
-		std::unique_lock<std::mutex> lg(m);
-		return sz;
-	}
-	T* pop() {
-		std::unique_lock<std::mutex> lg(m);
-		if (empty()) {
-			return nullptr;
-		}
-		auto ret = tab[begin];
-		tab[begin] = nullptr;
-		sz--;
-		begin = (begin + 1) % allocsize;
-		return ret;
-	}
-	bool push(T* elt) {
-		std::unique_lock<std::mutex> lg(m);
-		if (full()) {
-			return false;
-		}
-		tab[(begin + sz) % allocsize] = elt;
-		sz++;
-		return true;
-	}
-	~Queue() {
-		// ?? lock a priori inutile, ne pas detruire si on travaille encore avec
-		for (size_t i = 0; i < sz; i++) {
-			auto ind = (begin + i) % allocsize;
-			delete tab[ind];
-		}
-		delete[] tab;
-	}
-};
+        bool empty() const {
+            return sz == 0;
+        }
+        bool full() const {
+            return sz == allocsize;
+        }
+
+    public:
+
+        Queue(size_t size) :allocsize(size), begin(0), sz(0),isBlocking(true) {
+            tab = new T*[size];
+            for(size_t i = 0; i < size; ++i) {
+                tab[i] = nullptr;
+            }
+        }
+
+        size_t size() const {
+            std::unique_lock<std::mutex> lg(m);
+            return sz;
+        }
+
+        T* pop() {
+            std::unique_lock<std::mutex> lg(m);
+            while (isBlocking && empty()) {
+                cond_vide.wait(lg);
+            }
+            if (empty()) {
+                return nullptr;
+            }
+            auto ret = tab[begin];
+            tab[begin] = nullptr;
+            sz--;
+            begin = (begin + 1) % allocsize;
+            cond_plein.notify_all();
+            return ret;
+        }
+
+        bool push(T* elt) {
+            std::unique_lock<std::mutex> lg(m);
+            while (isBlocking && full()) {
+                cond_plein.wait(lg);
+            }
+            if (full()) {
+                return false;
+            }
+            tab[(begin + sz) % allocsize] = elt;
+            sz++;
+            cond_vide.notify_all();
+            return true;
+        }
+
+        void setBlocking(bool block) {
+            std::unique_lock<std::mutex> lg(m);
+            isBlocking = block;
+            if (!isBlocking) {
+                cond_vide.notify_all();
+                cond_plein.notify_all();
+            }
+        }
+
+        ~Queue() {
+            for (size_t i = 0; i < sz; i++) {
+                auto ind = (begin + i) % allocsize;
+                delete tab[ind];
+            }
+            delete[] tab;
+        }
+    };
 
 }
 
--- TME5/src/main.cpp
+++ TME5/src/main.cpp
@@ -7,6 +7,13 @@
 #include <limits>
 #include <random>
 
+
+#include "Queue.h"
+#include "Job.h"
+#include "Pool.h"
+#include " Barrier.h"
+
+
 using namespace std;
 using namespace pr;
 
@@ -32,7 +39,7 @@ void fillScene(Scene & scene, default_random_engine & re) {
 
 // return the index of the closest object in the scene that intersects "ray"
 // or -1 if the ray does not intersect any object.
-int findClosestInter(const Scene & scene, const Rayon & ray) {
+int pr::findClosestInter(const Scene & scene, const Rayon & ray) {
 	auto minz = std::numeric_limits<float>::max();
 	int targetSphere = -1;
 	int index = 0;
@@ -51,7 +58,7 @@ int findClosestInter(const Scene & scene, const Rayon & ray) {
 
 // Calcule l'angle d'incidence du rayon à la sphere, cumule l'éclairage des lumières
 // En déduit la couleur d'un pixel de l'écran.
-Color computeColor(const Sphere & obj, const Rayon & ray, const Vec3D & camera, std::vector<Vec3D> & lights) {
+Color pr::computeColor(const Sphere & obj, const Rayon & ray, const Vec3D & camera, std::vector<Vec3D> & lights) {
 	Color finalcolor = obj.getColor();
 
 	// calcul du rayon et de sa normale a la sphere
@@ -101,63 +108,79 @@ void exportImage(const char * path, size_t width, size_t height, Color * pixels)
 // NB : en francais pour le cours, preferez coder en english toujours.
 // pas d'accents pour eviter les soucis d'encodage
 
-int main () {
+int main() {
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    default_random_engine re(std::chrono::system_clock::now().time_since_epoch().count());
+    Scene scene(1000, 1000);
+    fillScene(scene, re);
 
-	std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-	// on pose une graine basee sur la date
-	default_random_engine re(std::chrono::system_clock::now().time_since_epoch().count());
-	// definir la Scene : resolution de l'image
-	Scene scene (1000,1000);
-	// remplir avec un peu d'aléatoire
-	fillScene(scene, re);
-	
-	// lumieres 
-	vector<Vec3D> lights;
-	lights.reserve(3);
-	lights.emplace_back(Vec3D(50, 50, -50));
-	lights.emplace_back(Vec3D(50, 50, 120));
-	lights.emplace_back(Vec3D(200, 0, 120));
+    vector<Vec3D> lights;
+    lights.reserve(3);
+    lights.emplace_back(Vec3D(50, 50, -50));
+    lights.emplace_back(Vec3D(50, 50, 120));
+    lights.emplace_back(Vec3D(200, 0, 120));
 
-	// les points de l'ecran, en coordonnées 3D, au sein de la Scene.
-	// on tire un rayon de l'observateur vers chacun de ces points
-	const Scene::screen_t & screen = scene.getScreenPoints();
+    const Scene::screen_t& screen = scene.getScreenPoints();
+    Color* pixels = new Color[scene.getWidth() * scene.getHeight()];
 
-	// Les couleurs des pixels dans l'image finale
-	Color * pixels = new Color[scene.getWidth() * scene.getHeight()];
+//    // pour chaque pixel, calculer sa couleur
+//    for (int x =0 ; x < scene.getWidth() ; x++) {
+//        for (int  y = 0 ; y < scene.getHeight() ; y++) {
+//            // le point de l'ecran par lequel passe ce rayon
+//            auto & screenPoint = screen[y][x];
+//            // le rayon a inspecter
+//            Rayon  ray(scene.getCameraPos(), screenPoint);
+//
+//            int targetSphere = findClosestInter(scene, ray);
+//
+//            if (targetSphere == -1) {
+//                // keep background color
+//                continue ;
+//            } else {
+//                const Sphere & obj = *(scene.begin() + targetSphere);
+//                // pixel prend la couleur de l'objet
+//                Color finalcolor = computeColor(obj, ray, scene.getCameraPos(), lights);
+//                // le point de l'image (pixel) dont on vient de calculer la couleur
+//                Color & pixel = pixels[y*scene.getHeight() + x];
+//                // mettre a jour la couleur du pixel dans l'image finale.
+//                pixel = finalcolor;
+//            }
+//
+//        }
+//    }
 
-	// pour chaque pixel, calculer sa couleur
-	for (int x =0 ; x < scene.getWidth() ; x++) {
-		for (int  y = 0 ; y < scene.getHeight() ; y++) {
-			// le point de l'ecran par lequel passe ce rayon
-			auto & screenPoint = screen[y][x];
-			// le rayon a inspecter
-			Rayon  ray(scene.getCameraPos(), screenPoint);
 
-			int targetSphere = findClosestInter(scene, ray);
+    pr::Pool pool(100);
+    pool.start(20);
 
-			if (targetSphere == -1) {
-				// keep background color
-				continue ;
-			} else {
-				const Sphere & obj = *(scene.begin() + targetSphere);
-				// pixel prend la couleur de l'objet
-				Color finalcolor = computeColor(obj, ray, scene.getCameraPos(), lights);
-				// le point de l'image (pixel) dont on vient de calculer la couleur
-				Color & pixel = pixels[y*scene.getHeight() + x];
-				// mettre a jour la couleur du pixel dans l'image finale.
-				pixel = finalcolor;
-			}
 
-		}
-	}
+//    Ex9
+//    for (int x = 0; x < scene.getWidth(); x++) {
+//        for (int y = 0; y < scene.getHeight(); y++) {
+//            Color* pixel = &pixels[y * scene.getHeight() + x];
+//            pr::PixelJob* job = new pr::PixelJob(x, y, pixel, &scene, &lights);
+//            pool.submit(job);
+//        }
+//    }
 
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	    std::cout << "Total time "
-	              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
-	              << "ms.\n";
 
-	exportImage("toto.ppm",scene.getWidth(), scene.getHeight() , pixels);
+//      Ex10
+    for (int y = 0; y < scene.getHeight(); y++) {
+        Color* rowPixels = &pixels[y * scene.getWidth()];
+        pr::RowJob* job = new pr::RowJob(y, rowPixels, &scene, &lights);
+        pool.submit(job);
+    }
 
-	return 0;
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Total time "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+              << "ms.\n";
+
+    exportImage("toto.ppm", scene.getWidth(), scene.getHeight(), pixels);
+
+
+    pool.stop();
+    return 0;
 }
 
